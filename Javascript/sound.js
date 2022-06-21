77f881cd776b9bc385e5a0912475d4f41bcbe40f
Okoli Akachukwu <okoli555aka@gmail.com>
2022-06-21 17:00:11 +0100

    fully revamped the javascript file

--- Javascript/sound.js
+++ Javascript/sound.js
@@ -1,26 +1,28 @@
 var LIMIT = 1; // Allows the program to start playing the audio only once
 var audio = new Audio('/Sound Effects/backgroundmusic.mp3');
 
-function sound() {
+for (let i = 0; i < 3; i++ ){
+    document.getElementsByClassName("sound")[i].addEventListener("click", () => {
+        if (LIMIT == 1){
+            audio.play();
+            audio.loop = true;
+            LIMIT += 1;
+        }
     
-    if (LIMIT == 1){
-        audio.play();
-        audio.loop = true;
-        LIMIT += 1;
-    }
-
-    if (icon.classList[1] == "fa-volume-xmark") {
-        icon.classList.replace("fa-volume-xmark", "fa-volume-high")
-        icon1.classList.replace("fa-volume-xmark", "fa-volume-high")
-        icon2.classList.replace("fa-volume-xmark", "fa-volume-high")
-        icon3.classList.replace("fa-volume-xmark", "fa-volume-high")
-        audio.muted = false;
-    } else {
-        icon.classList.replace("fa-volume-high", "fa-volume-xmark")
-        icon1.classList.replace("fa-volume-high", "fa-volume-xmark")
-        icon2.classList.replace("fa-volume-high", "fa-volume-xmark")
-        icon3.classList.replace("fa-volume-high", "fa-volume-xmark")
-        audio.muted = true;
-    }
+        if (icon.classList[1] == "fa-volume-xmark") {
+            icon.classList.replace("fa-volume-xmark", "fa-volume-high")
+            icon1.classList.replace("fa-volume-xmark", "fa-volume-high")
+            icon2.classList.replace("fa-volume-xmark", "fa-volume-high")
+            icon3.classList.replace("fa-volume-xmark", "fa-volume-high")
+            audio.muted = false;
+        } else {
+            icon.classList.replace("fa-volume-high", "fa-volume-xmark")
+            icon1.classList.replace("fa-volume-high", "fa-volume-xmark")
+            icon2.classList.replace("fa-volume-high", "fa-volume-xmark")
+            icon3.classList.replace("fa-volume-high", "fa-volume-xmark")
+            audio.muted = true;
+        }
+    })
 }
+
 
--- Javascript/whackamole.js
+++ Javascript/whackamole.js
@@ -1,7 +1,7 @@
-var playy;
-var pausee;
+let playy;
+let pausee;
 
-var counter = 0;
+let counter = 0;
 
 const mole = ["mole1", "mole2", "mole3", "mole4", "mole5", "mole6", "mole7", "mole8", "mole9"];
 
@@ -26,7 +26,7 @@ const moles = (num) => {
 }
 
 
-function popping(){
+const popping = () => {
     let num;
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     
@@ -77,25 +77,24 @@ document.getElementById("counter").innerHTML = counter;
 document.getElementById("output").innerHTML = counter;
 }
 
-var time; /* Use this to set timelimit*/
-var count;
-var difficulty;  /* Use this to set how fast the mole will be spawned (difficulty*/
-var counts;
-var timeds;
-var highscore = [];
-var popss;
-var difficult = document.getElementById("difficulty");
-var easy1 = document.getElementById("easy");
-var medium1 = document.getElementById("medium");
-var hard1 = document.getElementById("hard");
-var thirty1 = document.getElementById("thirty");
-var sixty1 = document.getElementById("sixty");
-var onetwenty1 = document.getElementById("onetwenty");
-var twoforty1 = document.getElementById("twoforty");
-var oneeighty1 = document.getElementById("oneeighty");
+let time; /* Use this to set timelimit*/
+let count;
+let difficulty;  /* Use this to set how fast the mole will be spawned (difficulty*/
+let counts;
+let timeds;
+let highscore = [];
+let popss;
+let difficult = document.getElementById("difficulty");
+let easy1 = document.getElementById("easy");
+let medium1 = document.getElementById("medium");
+let hard1 = document.getElementById("hard");
+let thirty1 = document.getElementById("thirty");
+let sixty1 = document.getElementById("sixty");
+let onetwenty1 = document.getElementById("onetwenty");
+let twoforty1 = document.getElementById("twoforty");
+let oneeighty1 = document.getElementById("oneeighty");
 
-
-function begin (){
+document.getElementById("begin").addEventListener("click", () => {
     if (count == undefined || counts == undefined){
         alert("Please select both a difficulty level and timelimit")
     } else {
@@ -108,67 +107,66 @@ function begin (){
         document.getElementById("main").style.visibility = "visible";
         selection(); /*This function set a difficulty level under the high scores*/
     }
-}
+})
 
-
-function pause(){   /* Function for the pause button */
+document.getElementById("pause").addEventListener("click", () => {
     pausee = true;
-}
+})
 
-function play(){    /* Function for the play button */
+document.getElementById("play").addEventListener("click", () => {
     pausee = false;
-}
+})
 
-function start(){
+document.getElementById("start").addEventListener("click", () => {
     document.getElementById("introduction").style.visibility = "hidden";
     document.getElementById("instructions").style.visibility = "visible";
-}
+})
 
-function next(){
+document.getElementById("next").addEventListener("click", () => {
     document.getElementById("instructions1").style.visibility = "hidden";
     document.getElementById("instructions12").style.visibility = "visible";
-}
+})
 
-function previous(){
+document.getElementById("previous").addEventListener("click", () => {
     document.getElementById("instructions12").style.visibility = "hidden";
     document.getElementById("instructions1").style.visibility = "visible";
-}
+})
 
-function easy(){
+easy1.addEventListener("click", () => {
     easy1.classList.add("change");
     medium1.classList.remove("change");
     hard1.classList.remove("change");
     counts = 1;
     difficulty = 1000;
-}
+})
 
-function medium(){
+medium1.addEventListener("click", () => {
     easy1.classList.remove("change");
     medium1.classList.add("change");
     hard1.classList.remove("change");
     counts = 2;
     difficulty = 750;
-}
+})
 
-function hard(){
+hard1.addEventListener("click", () => {
     easy1.classList.remove("change");
     medium1.classList.remove("change");
     hard1.classList.add("change");
     counts = 3;
     difficulty = 350;
-}
+})
 
-function thirty(){
+thirty1.addEventListener("click", () => {
     thirty1.classList.add("change");
     sixty1.classList.remove("change");
     onetwenty1.classList.remove("change");
     oneeighty1.classList.remove("change");
     twoforty1.classList.remove("change");
     count = 1;
-    time = 30; 
-}
+    time = 30;
+})
 
-function sixty(){
+sixty1.addEventListener("click", () => {
     thirty1.classList.remove("change");
     sixty1.classList.add("change");
     onetwenty1.classList.remove("change");
@@ -176,9 +174,9 @@ function sixty(){
     twoforty1.classList.remove("change");
     count = 2;
     time = 60; 
-}
+})
 
-function onetwenty(){
+onetwenty1.addEventListener("click", () => {
     thirty1.classList.remove("change");
     sixty1.classList.remove("change");
     onetwenty1.classList.add("change");
@@ -186,9 +184,9 @@ function onetwenty(){
     twoforty1.classList.remove("change");
     count = 3;
     time = 120; 
-}
+})
 
-function oneeighty(){
+oneeighty1.addEventListener("click", () => {
     thirty1.classList.remove("change");
     sixty1.classList.remove("change");
     onetwenty1.classList.remove("change");
@@ -196,68 +194,63 @@ function oneeighty(){
     twoforty1.classList.remove("change");
     count = 4;
     time = 180; 
-}
+})
 
-function twoforty(){
+twoforty1.addEventListener("click", () => {
     thirty1.classList.remove("change");
     sixty1.classList.remove("change");
     onetwenty1.classList.remove("change");
     oneeighty1.classList.remove("change");
     twoforty1.classList.add("change");
     count = 5;
-    time = 240; 
-}
+    time = 240;
+})
 
-function reset(){
+const reset = () => {
     difficulty = undefined;
     count = undefined;
     counts = undefined;
     time = undefined;
     counter = 0;
+    const scores = ["score1", "score2", "score3", "score4", "score5", "score6", "score7", "score8", "score9", "score10"];
+    const timeAndDifficulty = [thirty1, sixty1, onetwenty1, oneeighty1, twoforty1, easy1, medium1, hard1];
 
-    document.getElementById("score1").innerHTML = ""
-    document.getElementById("score2").innerHTML = ""
-    document.getElementById("score3").innerHTML = ""
-    document.getElementById("score4").innerHTML = ""
-    document.getElementById("score5").innerHTML = ""
-    document.getElementById("score6").innerHTML = ""
-    document.getElementById("score7").innerHTML = ""
-    document.getElementById("score8").innerHTML = ""
-    document.getElementById("score9").innerHTML = ""
-    document.getElementById("score10").innerHTML = ""
+    for(let i = 0; i < scores.length; i++){
+        document.getElementById(scores[i]).innerHTML = ""; 
+    }
+
+    for (let i = 0; i < timeAndDifficulty.length; i++){
+        timeAndDifficulty[i].classList.remove("change");
+    }
+
     highscore = [];
-
-    thirty1.classList.remove("change");
-    sixty1.classList.remove("change");
-    onetwenty1.classList.remove("change");
-    oneeighty1.classList.remove("change");
-    twoforty1.classList.remove("change");
-
-    easy1.classList.remove("change");
-    medium1.classList.remove("change");
-    hard1.classList.remove("change");
 }
 
-function mainmenu(){
+const mainmenu = () => {
     reset();
     document.getElementById("instructions").style.visibility = "visible";
     document.getElementById("instructions12").style.visibility = "visible";
 }
 
-function mainmenu2(){
+document.getElementById("mainmenu").addEventListener("click", () => {
     mainmenu();
-    clearInterval (timeds);
-    clearInterval (popss);
-}
+})
 
-function end(){
+document.getElementById("mainmenu2").addEventListener("click", () => {
+    mainmenu();
+    clearInterval(timeds);
+    clearInterval(popss);
+})
+
+document.getElementById("end").addEventListener("click", () => {
     reset();
     document.getElementById("introduction").style.visibility = "visible";
     document.getElementById("instructions").style.visibility = "visible";
     document.getElementById("instructions1").style.visibility = "visible";
-}
+})
 
-function timer() {
+
+const timer = () => {
     if (time > 0 && pausee != true) {
         time -=1;
         document.getElementById("timer").innerHTML = time;
@@ -265,12 +258,12 @@ function timer() {
     if (time == 0) {
         clearInterval (timeds);
         clearInterval (popss);
-        document.getElementById("results").style.visibility = "visible"
+        document.getElementById("results").style.visibility = "visible";
         highscores();
     }
 }
 
-function retry(){
+document.getElementById("retry").addEventListener("click", () => {
     document.getElementById("results").style.visibility = "hidden";
     counter = 0;
 
@@ -305,49 +298,22 @@ function retry(){
     }
 
     begin();
-}
+})
 
-function music2(){
-    audio2.play();
-}
-
-function highscores(){
+const highscores = () => {
     highscore.push(counter);
     highscore = highscore.sort((a,b)=>b-a);
 
-    if (highscore[0] != undefined){
-        document.getElementById("score1").innerHTML = highscore[0];
-    }
-    if (highscore[1] != undefined){
-        document.getElementById("score2").innerHTML = highscore[1];
-    }
-    if (highscore[2] != undefined){
-        document.getElementById("score3").innerHTML = highscore[2];
-    }
-    if (highscore[3] != undefined){
-        document.getElementById("score4").innerHTML = highscore[3];
-    }
-    if (highscore[4] != undefined){
-        document.getElementById("score5").innerHTML = highscore[4];
-    }
-    if (highscore[5] != undefined){
-        document.getElementById("score6").innerHTML = highscore[5];
-    }
-    if (highscore[6] != undefined){
-        document.getElementById("score7").innerHTML = highscore[6];
-    }
-    if (highscore[7] != undefined){
-        document.getElementById("score8").innerHTML = highscore[7];
-    }
-    if (highscore[8] != undefined){
-        document.getElementById("score9").innerHTML = highscore[8];
-    }
-    if (highscore[9] != undefined){
-        document.getElementById("score10").innerHTML = highscore[9];
+    const scores = ["score1", "score2", "score3", "score4", "score5", "score6", "score7", "score8", "score9", "score10"];
+
+    for (let i = 0; i < scores.length; i++){
+        if (highscore[i] != undefined){
+            document.getElementById(scores[i]).innerHTML = highscore[i]
+        }
     }
 }
 
-function selection(){
+const selection = () => {
     switch(counts){
         case 1: 
             difficult.innerHTML = "(Easy)";
